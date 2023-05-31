0553199eda1ba15a788566841cd64fb141e2707b
wojtekczajka <w.czajka@gmail.com>
2023-05-31 08:52:01 +0200

    server program update

--- server.cpp
+++ server.cpp
@@ -9,29 +9,29 @@ using namespace std;
 using namespace Chat;
 
 int main(int argc, char* argv[]) {
+    int status = 0;
+    Ice::CommunicatorPtr ic;
     try {
-        Ice::CommunicatorHolder ich(argc, argv);
-        auto adapter = ich->createObjectAdapterWithEndpoints("ChatAdapter", "default -h localhost -p 10000");
+        ic = Ice::initialize(argc, argv);
+        Ice::ObjectAdapterPtr adapter =
+            ic->createObjectAdapterWithEndpoints("ChatAdapter", "default -h localhost -p 10000");
         auto lobby = new LobbyI();
-
-        // Register the lobby servant with the adapter
+        
         adapter->add(lobby, Ice::stringToIdentity("Lobby"));
-
         adapter->activate();
-        ich->waitForShutdown();
+        ic->waitForShutdown();
     } catch (const Ice::Exception& ex) {
         cerr << ex << endl;
         return 1;
     }
 
-    // if (ich) {
-    //     try {
-    //         ich->destroy();
-    //     } catch (const std::exception& e) {
-    //         std::cerr << e.what() << '\n';
-    //         return 1;
-    //     }
-    // }
-
-    return 0;
+    if (ic) {
+        try {
+            ic->destroy();
+        } catch (const Ice::Exception& e) {
+            cerr << e << endl;
+            status = 1;
+        }
+    }
+    return status;
 }
